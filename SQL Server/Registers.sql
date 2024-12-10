d543f6a3856d622daeefb2f7b126f5440ffb14af
Maritza Santibañez <L22420423@jiquilpan.tecnm.mx>
2024-12-10 00:17:10 -0600

    Registros tabla trabajadores

---
+++ SQL Server/Registers.sql
@@ -0,0 +1,50 @@
+--Registros de la tabla trabajadores (workers)----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Jovita Lizeth', 'Galván', 'Rojas', 'GARJ980704MMNLJV01', '1998-07-04', 'Femenino', 'Soltera', 'Dámaso Cárdenas', '187', 'Centro', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Licenciatura', '2022-01-13', 'Jefe', '500.0', 'Permanente', '3531007723', 'jovisg.98@gmail.com', '1', '598654841');
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Mitzu Sayin', 'Galván', 'Rojas', 'GARM951019MMNLJT0', '1998-10-19', 'Femenino', 'Casada', 'Dámaso Cárdenas', '187', 'Centro', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Licenciatura', '2022-10-10', 'Gerente', '300.0', 'Anual', '3531233538', 'galvanmit@gmail.com', '1', '598471020')
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Israel', 'Galván', 'Rojas', 'GARI050801HJCLJSA3', '2005-08-01', 'Masculino', 'Soltero', 'Dámaso Cárdenas', '187', 'Centro', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Preparatoria', '2023-01-03', 'Vendedor', '250.0', 'Trimestral', '3532053638', 'galvanisr@gmail.com', '1', '598300621')
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Sofia', 'García', 'Hernández', 'GAHS040810JIULHGS2', '2004-08-10', 'Femenino', 'Soltero', 'Heriberto Jara', '485', 'La Forestal', 
+	'Sahuayo', 'Michoacán', 'México', '59050', 'Preparatoria', '2023-04-30', 'Vendedor', '250.0', 'Temporal', '3531234567', 'garciasof@gmail.com', '0', '587693159')
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Juan Pablo', 'López', 'Ramírez', 'LRJP930130YGSRFCY4', '1993-01-30', 'Femenino', 'Casado', 'Av. Sierra', '456', 'Valle Verde', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Maestría', '2022-12-10', 'Vendedor', '250.0', 'Trimestral', '3537890123', 'lopezjupa@gmail.com', '1', '489100310')
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('María Fernanda', 'Rodríguez', 'Gómez', 'RGMA991021KJHYUAS8', '1999-10-21', 'Femenino', 'Soltero', 'Vicente Guerrero', '758', 'La Forestal', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Licenciatura', '2022-12-28', 'Vendedor', '250.0', 'Mensual', '3532345678', 'rodriguezmar@gmail.com', '1', '651302178')
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Carlos Eduardo', 'Martínez', 'Sánchez', 'MSCE000415NMGADET5', '2000-04-15', 'Masculino', 'Soltero', 'Av. Independencia', '568', 'San Onofre', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Licenciatura', '2023-05-03', 'Vendedor', '250.0', 'Mensual', '3532456789', 'martinezcared@gmail.com', '0', '687103249')
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Ana Karina', 'Velasco', 'Morales', 'VMAK890529KILAEBT8', '1989-05-29', 'Femenino', 'Casado', 'Vallarta', '537', 'Las Brisas', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Maestría', '2023-09-28', 'Gerente', '300.0', 'Anual', '3539012345', 'velascoanakar@gmail.com', '1', '5887469100')
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Luis Enrique', 'Álvarez', 'Reyes', 'ARLE991212KIULOSR9', '1999-12-12', 'Masculino', 'Soltero', 'Av. Lázaro Cárdenas', '568', 'Centro', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Licenciatura', '2024-01-23', 'Vendedor', '250.0', 'Mensual', '3535678901', 'alvarezluen@gmail.com', '1', '001690354')
+
+INSERT INTO trabajadores(nombre, apellido_paterno, apellido_materno, curp, fecha_nacimiento, genero, estado_civil, calle, numero,  colonia,
+	ciudad, estado, pais, codigo_postal, nivel_educativo, fecha_contratacion, puesto, salario_base, tipo_contrato, telefono, email, estatus, numero_seguro_social)
+VALUES('Daniela Alejandra', 'Torres', 'Ortiz', 'TODA981225KIULMOA1', '1998-12-25', 'Femenino', 'Casado', 'Calzada Revolución', '846', 'Valle Verde', 
+	'Sahuayo', 'Michoacán', 'México', '59000', 'Preparatoria', '2024-05-03', 'Vendedor', '250.0', 'Temporal', '3531115897', 'torresdan@gmail.com', '1', '589631024')
