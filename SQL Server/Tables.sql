622b5d58b21f4aa15b093027c39deb01353561f9
Miguel Romero <mickey260504@gmail.com>
2024-12-10 00:23:48 -0600

    Tabla de proveedores

--- SQL Server/Tables.sql
+++ SQL Server/Tables.sql
@@ -14,4 +14,24 @@ CREATE TABLE trabajadores(
 	referencias VARCHAR(100)
 )
 
+-- Creación de la tabla proveedores (suppliers) -----------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE TABLE proveedores(
+	id_proveedor INTEGER IDENTITY (1,1),
+	nombre VARCHAR(20) NOT NULL,
+	apellido_paterno VARCHAR (20) NOT NULL,
+	apellido_materno VARCHAR (20),
+	telefono VARCHAR (10) NOT NULL,
+	email VARCHAR (30) NOT NULL,
+	calle VARCHAR (25) NOT NULL,
+	numero INTEGER NOT NULL, 
+	colonia VARCHAR (20),
+	ciudad VARCHAR (20) NOT NULL,
+	estado VARCHAR(20),
+	pais VARCHAR (20),
+	codigo_postal VARCHAR (10),
+	observaciones VARCHAR(100),
+	URL_sitio_web VARCHAR (100),
+	nombre_compañia VARCHAR (40) NOT NULL,
+	estatus VARCHAR(5)
+)
 
