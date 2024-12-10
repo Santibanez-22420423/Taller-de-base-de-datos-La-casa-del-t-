6f48300b0b5abb0dc7f75551761376ec472e05f6
Miguel Romero <mickey260504@gmail.com>
2024-12-10 01:38:36 -0600

    Triggers tabla devoluciones_compras

--- SQL Server/Triggers.sql
+++ SQL Server/Triggers.sql
@@ -107,3 +107,83 @@ BEGIN
   SET subtotal = cantidad * unitario
   WHERE id_producto = @id_producto;
 END;
+
+--Triggers tabla devoluciones_compras-------------------------------------------------------------------------------------------------------------
+-- Verificar que exista la cantidad a devolver de la compra realizada.
+
+create trigger trg_cantidad_devolucion_compra
+on devoluciones_compras
+instead of insert
+as
+begin  
+    declare @id_producto integer, @cantidad integer, @comprados integer, @id_compra integer;
+
+    -- Obtener el id_producto y la cantidad de la tabla inserted que son los datos de devoluciones_compras.
+    select @id_producto = i.id_producto, @cantidad = i.cantidad, @id_compra = i.id_compra
+    from inserted i;
+
+
+	--saca la cantidad desde la tabla de productos_compras para verificar que no sea mayor a la cantidad comprada.
+    -- Obtener la cantidad de productos de la compra.
+    select @comprados = cantidad
+    from productos_compras
+    where id_producto = @id_producto and id_compra = @id_compra;
+
+    -- Verificar si hay suficiente existencia.
+    if @cantidad <= @comprados
+    begin
+        -- Si hay comprados suficientes para la devolución, realizar la inserción.
+        insert into devoluciones_compras ( id_compra, id_producto, fecha, cantidad, unitario)
+        select id_compra, id_producto, fecha, cantidad, unitario
+        from inserted;
+
+        -- Actualizar la existencia del producto.
+        update productos
+        set cantidad_stock = cantidad_stock - @cantidad
+        where id_producto = @id_producto;
+    end
+    else
+    begin
+        -- Si no hay suficiente cantidad de comprados para la devolución, lanzar un error.
+        raiserror('No es posible realizar la devolución sobre compra: cantidad a devolver excede la cantidad comprada.', 16, 1);
+    end
+end;
+
+--Calcular el valor del reembolso.
+  CREATE TRIGGER trg_calcular_reembolso
+	ON devoluciones_compras
+	AFTER INSERT
+	AS
+	BEGIN
+		-- Actualizar el reembolso en devoluciones_compras
+		UPDATE devoluciones_compras
+		SET reembolso = i.cantidad * i.unitario
+		FROM devoluciones_compras
+		JOIN inserted i ON devoluciones_compras.id_producto = i.id_producto;
+	END;
+
+--Pasar el valor de costo de la tabla productos asignándolo a el atributo unitario de la tabla devoluciones_compras.
+
+CREATE TRIGGER trg_DarAUnitarioValorDevCompra
+ON devoluciones_compras
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @id_producto INTEGER, @unitario MONEY, @cantidad INTEGER;
+
+    -- Obtener el id_producto y la cantidad de la tabla inserted
+    SELECT @id_producto = i.id_producto, @cantidad = i.cantidad
+    FROM inserted i;
+
+    -- Obtener el costo actual del producto
+    SELECT @unitario = costo_venta
+    FROM productos
+    WHERE id_producto = @id_producto;
+
+    -- Actualizar la tabla devoluciones_compras con el reembolso y el unitario
+    UPDATE dc
+    SET dc.reembolso = @unitario * @cantidad, 
+        dc.unitario = @unitario
+    FROM devoluciones_compras dc
+    INNER JOIN inserted i ON dc.id_devolucion_compra = i.id_devolucion_compra;
+END;
